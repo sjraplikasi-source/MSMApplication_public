eb7981fd0a4f86a5210c5eca57251b8fa68f28b5
sjraplikasi-source <sjr.aplikasi@gmail.com>
2025-10-19 17:41:41 +0800

    Updated Settings.tsx

---
+++ src/pages/MineMaintenance/Settings.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MineSettings() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-4">Maintenance Settings</h1>
+      <p>Pengaturan interval dan threshold maintenance mining.</p>
+    </div>
+  );
+}
