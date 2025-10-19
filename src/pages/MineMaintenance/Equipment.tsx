1be80a31579cd28883d9114a2baced50116e8d6f
sjraplikasi-source <sjr.aplikasi@gmail.com>
2025-10-19 17:41:05 +0800

    Updated Equipment.tsx

---
+++ src/pages/MineMaintenance/Equipment.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MineEquipment() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-4">Equipment List (Mining)</h1>
+      <p>Data equipment akan diambil dari tabel <code>equipment</code> (source='Mine').</p>
+    </div>
+  );
+}
