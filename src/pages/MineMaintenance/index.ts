3adcaec145d5ae256f5e116aa6f4eab192a1bef6
sjraplikasi-source <sjr.aplikasi@gmail.com>
2025-10-19 17:40:45 +0800

    Updated index.ts

---
+++ src/pages/MineMaintenance/index.ts
@@ -0,0 +1,5 @@
+export { default as MineDashboard } from "./Dashboard";
+export { default as MineEquipment } from "./Equipment";
+export { default as MineSchedule } from "./Schedule";
+export { default as MineTools } from "./Tools";
+export { default as MineSettings } from "./Settings";
