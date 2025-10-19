0fc953a9e68e5278968e80525cc8d5ee2744f0c1
sjraplikasi-source <sjr.aplikasi@gmail.com>
2025-10-19 17:41:32 +0800

    Updated Tools.tsx

---
+++ src/pages/MineMaintenance/Tools.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MineTools() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-4">Tools & Loans</h1>
+      <p>Menampilkan data tools dan peminjaman alat dari tabel <code>tools</code> & <code>tool_loans</code>.</p>
+    </div>
+  );
+}
