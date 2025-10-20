71f275c891313f6ea7e749beaa294e0a6a31ab23
sjraplikasi-source <sjr.aplikasi@gmail.com>
2025-10-20 12:02:07 +0800

    Updated ReportForm.tsx

--- src/pages/ReportForm.tsx
+++ src/pages/ReportForm.tsx
@@ -4,7 +4,7 @@
 
 import React, { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { supabase } from '../lib/supabase';
+import { supabase } from '@/lib/supabase';
 import { useAuth } from '../context/AuthContext';
 import Select from 'react-select';
 import { useParams } from 'react-router-dom';
