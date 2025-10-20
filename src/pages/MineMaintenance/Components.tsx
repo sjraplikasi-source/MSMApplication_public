c9d3b988f9639e7e7dd65b5aa9fa86eaa951626c
sjraplikasi-source <sjr.aplikasi@gmail.com>
2025-10-20 13:19:11 +0800

    Updated Components.tsx

--- src/pages/MineMaintenance/Components.tsx
+++ src/pages/MineMaintenance/Components.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Search, Filter, Settings } from 'lucide-react';
 import { useMaintenanceContext } from '@/context/MaintenanceContext';
-import ComponentListItem from '../components/ComponentListItem';
+import ComponentListItem from "@/components/mine/ComponentListItem";
+
 
 const Components: React.FC = () => {
   const navigate = useNavigate();
