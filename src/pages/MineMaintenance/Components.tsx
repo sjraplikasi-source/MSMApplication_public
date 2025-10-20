51bc31757145510d0448f79f56be25415434dea2
sjraplikasi-source <sjr.aplikasi@gmail.com>
2025-10-20 11:53:33 +0800

    Updated Components.tsx

--- src/pages/MineMaintenance/Components.tsx
+++ src/pages/MineMaintenance/Components.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Search, Filter, Settings } from 'lucide-react';
-import { useMaintenanceContext } from '../context/MaintenanceContext';
+import { useMaintenanceContext } from '@/context/MaintenanceContext';
 import ComponentListItem from '../components/ComponentListItem';
 
 const Components: React.FC = () => {
