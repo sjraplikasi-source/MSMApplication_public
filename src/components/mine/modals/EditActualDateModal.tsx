527ed75c196a38123b047f6586cfbef9ca36d3cd
sjraplikasi-source <sjr.aplikasi@gmail.com>
2025-10-20 13:34:33 +0800

    Updated EditActualDateModal.tsx

--- src/components/mine/modals/EditActualDateModal.tsx
+++ src/components/mine/modals/EditActualDateModal.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { supabase } from '@/lib/supabaseClient';
-import { generateNextWeeklyCheck } from '../../services/weeklyCheckScheduler';
+import { generateNextWeeklyCheck } from '@/services/weeklyCheckScheduler';
 
 interface Props {
   id: string;
