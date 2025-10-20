744e5a85650b6111296a49cca93560ecf700a1e7
sjraplikasi-source <sjr.aplikasi@gmail.com>
2025-10-20 11:34:41 +0800

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -14,9 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <MaintenanceProvider>
-        <App />
-      </MaintenanceProvider>
+      <App />
     </AuthProvider>
   </React.StrictMode>
 );
