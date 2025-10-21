59e76f2779883fe8f06be47dfde20de5661dc9c9
sjraplikasi-source <sjr.aplikasi@gmail.com>
2025-10-21 18:07:29 +0800

    Updated Dashboard.tsx

--- src/pages/ToolRoom/pages/Dashboard.tsx
+++ src/pages/ToolRoom/pages/Dashboard.tsx
@@ -116,7 +116,7 @@ export default function ToolRoomDashboard() {
 
       {/* Tombol Aksi */}
       <div className="flex gap-3 flex-wrap">
-        <Button onClick={() => navigate("/toolroom/")} >
+        <Button onClick={() => navigate("/toolroom/return-tools")} >
           List Available Tools
         </Button>
         <Button onClick={() => navigate("/toolroom/reports")} variant="outline">
