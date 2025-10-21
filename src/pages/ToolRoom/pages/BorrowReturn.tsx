6085e84b88390ac4b52e80c776a383ca74da762d
sjraplikasi-source <sjr.aplikasi@gmail.com>
2025-10-21 08:52:01 +0800

    Updated BorrowReturn.tsx

---
+++ src/pages/ToolRoom/pages/BorrowReturn.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function BorrowReturn() {
+  return (
+    <div>
+      <h2 className="text-xl font-semibold mb-2">Peminjaman & Pengembalian</h2>
+      <p>Halaman ini akan digunakan untuk mencatat aktivitas peminjaman dan pengembalian tools.</p>
+    </div>
+  );
+}
