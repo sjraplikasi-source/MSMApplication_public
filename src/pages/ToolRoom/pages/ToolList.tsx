4cea3c9077e65cd205ec4d91d6ab0e766a1d2191
sjraplikasi-source <sjr.aplikasi@gmail.com>
2025-10-21 08:51:51 +0800

    Updated ToolList.tsx

---
+++ src/pages/ToolRoom/pages/ToolList.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ToolList() {
+  return (
+    <div>
+      <h2 className="text-xl font-semibold mb-2">Daftar Tools</h2>
+      <p>Halaman ini akan menampilkan daftar semua tools di warehouse.</p>
+    </div>
+  );
+}
