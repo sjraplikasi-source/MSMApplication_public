269a020d4caa2aaad4e060475b7a605dfff5ff36
sjraplikasi-source <sjr.aplikasi@gmail.com>
2025-10-21 08:41:39 +0800

    Updated ToolRoomRouter.tsx

---
+++ src/pages/ToolRoom/ToolRoomRouter.tsx
@@ -0,0 +1,19 @@
+// src/pages/ToolRoom/ToolRoomRouter.tsx
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import ToolList from "./pages/ToolList";
+import BorrowReturn from "./pages/BorrowReturn";
+import Reports from "./pages/Reports";
+import ToolDetail from "./pages/ToolDetail";
+
+export default function ToolRoomRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="list" replace />} />
+      <Route path="list" element={<ToolList />} />
+      <Route path="borrow" element={<BorrowReturn />} />
+      <Route path="reports" element={<Reports />} />
+      <Route path="detail/:id" element={<ToolDetail />} />
+    </Routes>
+  );
+}
