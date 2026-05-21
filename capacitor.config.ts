4f10d8d5d049f248c8a84c0b9d7af9f8a6496b0c
sjraplikasi-source <sjr.aplikasi@gmail.com>
2026-05-21 10:06:20 +0800

    prepare apk

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.msm.application',
+  appName: 'MSM Application',
+  webDir: 'dist'
+  bundledWebRuntime: false,
+};
+
+export default config;
